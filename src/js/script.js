4fb89fe2f53d8439a9694d7c16deff36e8eed3bb
Kocherha Roman <f.your.d.2017@gmail.com>
2022-08-12 14:33:22 +0300

    adaptiv done

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,24 @@
 const hamburger = document.querySelector(".hamburger"),
   menu = document.querySelector(".menu"),
   closeElem = document.querySelector(".menu__close"),
-  sidepanel = document.querySelector('.sidepanel');
+  sidepanel = document.querySelector(".sidepanel");
 
 hamburger.addEventListener("click", () => {
   menu.classList.add("active");
-  hamburger.style.display = 'none';
-  sidepanel.style.display = 'none';
+  hamburger.style.display = "none";
+  sidepanel.style.display = "none";
 });
 
 closeElem.addEventListener("click", () => {
   menu.classList.remove("active");
-  hamburger.style.display = 'flex';
-  sidepanel.style.display = 'flex';
+  hamburger.style.display = "flex";
+  if (window.innerWidth > 1200) {
+    sidepanel.style.display = "flex";
+  }
 });
 
-const counters = document.querySelectorAll('.tools__ratings-counter'),
-      lines = document.querySelectorAll('.tools__ratings-line span');
+const counters = document.querySelectorAll(".tools__ratings-counter"),
+  lines = document.querySelectorAll(".tools__ratings-line span");
 
 counters.forEach((item, i) => {
   lines[i].style.width = item.innerHTML;
